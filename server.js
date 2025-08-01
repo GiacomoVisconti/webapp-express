a0f66ef3edcf1987531edea2ebfebf94cdf08950
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-08-01 15:59:21 +0200

    add cors protection to project

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const dotenv = require('dotenv')
 const movieRouter = require('./routes/moviesRouter')
 const erro404handler = require('./middlewares/error404handler')
 const erro500handler = require('./middlewares/error500handler')
+const cors = require('cors')
 
 //Server Listening
 app.listen(process.env.PORT, () => {
     console.log((`Server is running at http://localhost:${process.env.PORT}`));
 
 })
+
+app.use(cors())
 
 //Movie Routing
 app.use('/api/movies', movieRouter)
